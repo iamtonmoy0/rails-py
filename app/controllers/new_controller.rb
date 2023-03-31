ebfbde8a731f03a1337c6b65902a7ee53c04416c
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-31 17:44:52 -0400

    python added

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -1,4 +1,6 @@
 class NewController < ApplicationController
   def home
+our_input="hola"
+    @python=`python lib/assets/py/script.py "#{our_input}"`
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'new/home'
   resources :pages
-  root "posts#index"
+  root "new#home"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
